c682e2757abadc7207475c692d1c685c96474a49
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-06-26 16:04:10 +0200

    installation folder moved to .installation

--- installation/install_waybar_scrolling_mpris.sh
+++
@@ -1,7 +0,0 @@
-
-git clone https://github.com/sicista33/waybar-scrolling-mpris.git
-cd ./waybar-scrolling-mpris/build
-cmake ..
-make
-
-sudo mv waybar-scrolling-mpris ~/.config/waybar/waybar-scrolling-mpris
