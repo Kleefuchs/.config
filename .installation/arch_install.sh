e3be86a6f807edaf7b2471b13cbf5c53b0178d8a
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-06-26 16:32:51 +0200

    Added hyprswitch to arch_install.sh

--- .installation/arch_install.sh
+++ .installation/arch_install.sh
@@ -1,3 +1,3 @@
-yay -S waybar dunst playerctl pavucontrol hyprpaper swww hyprshot hyprlock wpctl wl-kbptr wlrctl hyfetch firedragon-bin mission-center brave-bin xfce4-thunar kitty hyprpolkitagent xdg-desktop-portal-hyprland brightnessctl wl-kbptr clipse-bin calc hyfetch hyprland neovim waybar-mpris-git cmake make jsonc yt-dlp kdeconnect
+yay -S waybar dunst playerctl pavucontrol hyprpaper swww hyprshot hyprlock hyprswitch wpctl wl-kbptr wlrctl hyfetch firedragon-bin mission-center brave-bin xfce4-thunar kitty hyprpolkitagent xdg-desktop-portal-hyprland brightnessctl wl-kbptr clipse-bin calc hyfetch hyprland neovim waybar-mpris-git cmake make jsonc yt-dlp kdeconnect
 
 bash install_waybar_scrolling_mpris.sh
