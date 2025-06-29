bdb8b7979cea34f376cae4d7eb1950e0938b019b
Julianos1b <matteojulianvetter@gmail.com>
2025-06-29 13:22:10 +0200

    new programs
    catpuccin overhaul for a lot of programs and a bit more programs in total aswell

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
 require("user")
 print("hello")
+local opts = {}
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+require("lazy").setup(plugins, opts)
+--require catppuccin
+require("catppuccin").setup()
+
+--set the colorscheme to it!
+vim.cmd.colorscheme "catppuccin"
