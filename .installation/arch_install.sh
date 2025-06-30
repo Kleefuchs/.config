02bfdb7fb2e8b7e4d81d8cb2cec7cf61ecaac8e9
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-06-30 13:46:43 +0200

    .installation

--- .installation/arch_install.sh
+++ .installation/arch_install.sh
@@ -1,4 +1,4 @@
-yay -S waybar dunst playerctl pavucontrol hyprpaper swww hyprshot hyprlock hyprswitch wpctl wl-kbptr wlrctl hyfetch firedragon-bin mission-center brave-bin xfce4-thunar kitty hyprpolkitagent xdg-desktop-portal-hyprland brightnessctl wl-kbptr clipse-bin calc hyfetch hyprland neovim waybar-mpris-git cmake make jsonc yt-dlp kdeconnect
+yay -S waybar dunst playerctl pavucontrol hyprpaper swww hyprshot hyprlock hyprswitch wpctl wl-kbptr wlrctl hyfetch firedragon-bin mission-center brave-bin xfce4-thunar kitty hyprpolkitagent xdg-desktop-portal-hyprland brightnessctl wl-kbptr clipse-bin calc hyfetch hyprland neovim waybar-mpris-git cmake make jsonc yt-dlp kdeconnect adwaita-fonts adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy libadwaita
 
 bash install_waybar_scrolling_mpris.sh
 bash install_hyprswitcher.sh
