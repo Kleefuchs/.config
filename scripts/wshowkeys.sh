e622414c90c0aea20896c65764092959628d06b7
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-07-18 20:14:58 +0200

    Updated wshowkeys.sh to be more silly

--- scripts/wshowkeys.sh
+++ scripts/wshowkeys.sh
@@ -6,5 +6,5 @@ if pgrep -x "$SERVICE" >/dev/null
 then
     killall wshowkeys
 else
-    wshowkeys -a bottom -F 'Sans Bold 30' -s '#B5B520ff' -f  '#ecd29cff' -b '#201B1488' -l 60
+    wshowkeys -a bottom -F 'Sans Bold 30' -s '#ff5050ff' -f  '#ffc0cbff' -b '#201B1488' -l 60
 fi
